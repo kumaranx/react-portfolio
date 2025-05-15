058d448ef0aed52416b7a9d7f5f0c15996b6a344
kumaranx <kumaran.0q@gmail.com>
2025-05-15 17:37:59 +0530

    Created Navbar and logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <>
-      <h1 className='text-4xl text-blue-300 font-bold'>Hola, Amingos!</h1>
+      <Navbar />
     </>
   )
 }
